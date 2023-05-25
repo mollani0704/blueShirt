12cef1314f528ffc7a163d8f0864808e8fac2c96
mollani0704 <mollani0704@gmail.com>
2023-05-25 15:34:20 +0900

    [Add] project init and setting

---
+++ src/main/java/com/project/blueshirt/BlueshirtApplication.java
@@ -0,0 +1,13 @@
+package com.project.blueshirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueshirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueshirtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/blueshirt/BlueshirtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.blueshirt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueshirtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
